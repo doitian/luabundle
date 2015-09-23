4f67153a3a34467acaa91eae19b40c5751b4d8c0
ian <ian@3pjgames.com>
2015-09-23 15:52:46 +0800

    Rename luarolls to luabundle

--- config.lua
+++ config.lua
@@ -1,1 +1,1 @@
-rocks_trees = { { root = "./rolls", bin_dir = "./rolls/bin", lib_dir = "./rolls/lib/lua", lua_dir = "./rolls/share/lua" } }
+rocks_trees = { { root = "./bundle", bin_dir = "./bundle/bin", lib_dir = "./bundle/lib/lua", lua_dir = "./bundle/share/lua" } }
