f81cf8541ae0288309a9feb52de0d420f0b1e1ca
ian <ian@3pjgames.com>
2015-11-19 12:58:42 +0800

    fix: add -fPIC in Linux

--- config.lua
+++ config.lua
@@ -1,2 +1,2 @@
 rocks_trees = { { root = "./bundle", bin_dir = "./bundle/bin", lib_dir = "./bundle/lib/lua", lua_dir = "./bundle/share/lua" } }
-variables = { CFLAGS = "-O2 -I./skynet/3rd/lua -I/usr/local/include", LIBFLAG = "{{LIBFLAG}} -L./skynet/3rd/lua -L/usr/local/lib", LUA = "./skynet/3rd/lua/lua", LUA_BINDIR = "./skynet/3rd/lua" }
+variables = { CFLAGS = "{{CFLAGS}} -I./skynet/3rd/lua -I/usr/local/include", LIBFLAG = "{{LIBFLAG}} -L./skynet/3rd/lua -L/usr/local/lib", LUA = "./skynet/3rd/lua/lua", LUA_BINDIR = "./skynet/3rd/lua" }
